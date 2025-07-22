f76fee08172ae200910c50e76c674da738eb185d
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2025-07-22 09:39:41 -0300

    evm: complete contracts inline documentation

--- packages/evm/contracts/interfaces/IExecutor.sol
+++ packages/evm/contracts/interfaces/IExecutor.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.20;
 
 import '../Intents.sol';
 
+/**
+ * @title Executor interface
+ */
 interface IExecutor {
+    /**
+     * @dev Executes an intent proposal
+     * @param intent Intent to be executed
+     * @param proposal Proposal to be executed to fulfill the intent
+     */
     function execute(Intent memory intent, Proposal memory proposal) external;
 }
