10f31f17b9bdd1d2dcf7c00e8b1ad8e70008b5dc
GuidoDipietro <dipietroguido@gmail.com>
2025-12-11 13:37:13 -0300

    Update sigs.rs for header checks

--- packages/svm/programs/settler/src/utils/sigs.rs
+++ packages/svm/programs/settler/src/utils/sigs.rs
@@ -23,13 +23,50 @@ pub fn get_args_from_ed25519_ix_data(data: &[u8]) -> Result<Ed25519Args<'_>> {
         return err!(SettlerError::SigVerificationFailed);
     }
 
-    let pubkey = data[16..16 + 32]
-        .try_into()
-        .map_err(|_| SettlerError::SigVerificationFailed)?;
-    let sig = data[48..48 + 64]
-        .try_into()
-        .map_err(|_| SettlerError::SigVerificationFailed)?;
+    // Header
+    let num_signatures = &[data[0]];
+    let padding = &[data[1]];
+    let signature_offset = &data[2..=3];
+    let signature_instruction_index = &data[4..=5];
+    let public_key_offset = &data[6..=7];
+    let public_key_instruction_index = &data[8..=9];
+    let message_data_offset = &data[10..=11];
+    let message_data_size = &data[12..=13];
+    let message_instruction_index = &data[14..=15];
+
+    // Data
+    let pubkey = &data[16..16 + 32];
+    let sig = &data[48..48 + 64];
     let msg = &data[112..];
 
-    Ok(Ed25519Args { pubkey, sig, msg })
+    // Expected values
+    let exp_public_key_offset: u16 = 16; // 2*u8 + 7*u16
+    let exp_signature_offset: u16 = exp_public_key_offset + 32_u16;
+    let exp_message_data_offset: u16 = exp_signature_offset + 64_u16;
+    let exp_num_signatures: u8 = 1;
+    let exp_message_data_size: u16 = msg.len().try_into().map_err(|_| SettlerError::SigVerificationFailed)?;
+
+    // Header
+    if num_signatures != &exp_num_signatures.to_le_bytes()
+        || padding != &[0]
+        || signature_offset != &exp_signature_offset.to_le_bytes()
+        || signature_instruction_index != &u16::MAX.to_le_bytes()
+        || public_key_offset != &exp_public_key_offset.to_le_bytes()
+        || public_key_instruction_index != &u16::MAX.to_le_bytes()
+        || message_data_offset != &exp_message_data_offset.to_le_bytes()
+        || message_data_size != &exp_message_data_size.to_le_bytes()
+        || message_instruction_index != &u16::MAX.to_le_bytes()
+    {
+        return err!(SettlerError::SigVerificationFailed);
+    }
+
+    Ok(Ed25519Args {
+        pubkey: pubkey
+            .try_into()
+            .map_err(|_| SettlerError::SigVerificationFailed)?,
+        sig: sig
+            .try_into()
+            .map_err(|_| SettlerError::SigVerificationFailed)?,
+        msg,
+    })
 }
