f1545be1a79e054c0dc1dc9396d0d0c64869da48
GuidoDipietro <dipietroguido@gmail.com>
2025-12-17 12:14:13 -0300

    Trim tests to this PRs features

--- packages/svm/programs/settler/src/lib.rs
+++ packages/svm/programs/settler/src/lib.rs
@@ -10,40 +10,14 @@ pub mod state;
 pub mod types;
 pub mod utils;
 
-use crate::{instructions::*, state::*, types::*};
+use crate::{instructions::*, types::*};
 
 #[program]
 pub mod settler {
     use super::*;
 
-    pub fn add_axia_sig(ctx: Context<AddAxiaSig>) -> Result<()> {
-        instructions::add_axia_sig(ctx)
-    }
-
-    pub fn add_instructions_to_proposal(
-        ctx: Context<AddInstructionsToProposal>,
-        more_instructions: Vec<ProposalInstruction>,
-        finalize: bool,
-    ) -> Result<()> {
-        instructions::add_instructions_to_proposal(ctx, more_instructions, finalize)
-    }
-
-    pub fn add_validator_sig(ctx: Context<AddValidatorSig>) -> Result<()> {
-        instructions::add_validator_sig(ctx)
-    }
-
-    pub fn change_whitelist_program(ctx: Context<ChangeWhitelistProgram>) -> Result<()> {
-        instructions::change_whitelist_program(ctx)
-    }
-
     pub fn claim_stale_intent(ctx: Context<ClaimStaleIntent>) -> Result<()> {
         instructions::claim_stale_intent(ctx)
-    }
-
-    pub fn claim_stale_proposal<'info>(
-        ctx: Context<'_, '_, 'info, 'info, ClaimStaleProposal<'info>>,
-    ) -> Result<()> {
-        instructions::claim_stale_proposal(ctx)
     }
 
     pub fn create_intent(
@@ -72,20 +46,6 @@ pub mod settler {
             deadline,
             is_final,
         )
-    }
-
-    pub fn create_proposal(
-        ctx: Context<CreateProposal>,
-        instructions: Vec<ProposalInstruction>,
-        fees: Vec<TokenFee>,
-        deadline: u64,
-        is_final: bool,
-    ) -> Result<()> {
-        instructions::create_proposal(ctx, instructions, fees, deadline, is_final)
-    }
-
-    pub fn execute_proposal(ctx: Context<ExecuteProposal>) -> Result<()> {
-        instructions::execute_proposal(ctx)
     }
 
     pub fn extend_intent(
--- packages/svm/programs/settler/src/state/mod.rs
+++ packages/svm/programs/settler/src/state/mod.rs
@@ -1,9 +1,7 @@
 pub mod fulfilled_intent;
 pub mod intent;
-pub mod proposal;
 pub mod settler_settings;
 
 pub use fulfilled_intent::*;
 pub use intent::*;
-pub use proposal::*;
 pub use settler_settings::*;
