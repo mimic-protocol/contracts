35a65450fa6e2c8860cee38f099a3d276e503b3c
GuidoDipietro <dipietroguido@gmail.com>
2026-03-31 12:22:59 -0300

    Abstract execute_transfer out to function

--- packages/svm/programs/settler/src/utils/execution/transfer.rs
+++ packages/svm/programs/settler/src/utils/execution/transfer.rs
@@ -4,10 +4,12 @@ use anchor_spl::{
     token_interface::{self, Mint as IMint, TokenAccount as ITokenAccount},
 };
 
+use core::slice::Iter;
+
 use crate::{
     errors::SettlerError,
     state::{Intent, Proposal},
-    types::SvmTransferIntentData,
+    types::{SvmTransfer, SvmTransferIntentData},
 };
 
 pub fn handle_transfer<'info>(
@@ -22,8 +24,8 @@ pub fn handle_transfer<'info>(
     validate_transfer(proposal, &decoded_intent_data)?;
 
     let delegate_seeds: &[&[u8]] = &[b"delegate", intent.user.as_ref(), &[delegate_bump]];
-    execute_transfer(
-        &intent.user,
+    execute_transfers(
+        intent.user,
         delegate,
         remaining_accounts,
         &decoded_intent_data,
@@ -34,7 +36,60 @@ pub fn handle_transfer<'info>(
 }
 
 fn execute_transfer<'info>(
-    user: &Pubkey,
+    transfer: &SvmTransfer,
+    delegate: &AccountInfo<'info>,
+    remaining_accounts_iter: &mut Iter<'_, AccountInfo<'info>>,
+    delegate_seeds: &[&[&[u8]]],
+    user: Pubkey,
+    token_program: &AccountInfo<'info>,
+    token_2022_program: &AccountInfo<'info>,
+) -> Result<()> {
+    let token_account_info = next_account_info(remaining_accounts_iter)?;
+    let recipient_account_info = next_account_info(remaining_accounts_iter)?;
+    let recipient_ta_account_info = next_account_info(remaining_accounts_iter)?;
+    let user_ta_account_info = next_account_info(remaining_accounts_iter)?;
+
+    let mut token_data: &[u8] = &token_account_info.try_borrow_data()?;
+    let token_mint = IMint::try_deserialize(&mut token_data)?;
+
+    let mut recipient_ta_data: &[u8] = &recipient_ta_account_info.try_borrow_data()?;
+    let recipient_ta = ITokenAccount::try_deserialize(&mut recipient_ta_data)?;
+
+    let mut user_ta_data: &[u8] = &user_ta_account_info.try_borrow_data()?;
+    let user_ta = ITokenAccount::try_deserialize(&mut user_ta_data)?;
+
+    let transfer_recipient = Pubkey::try_from(transfer.recipient.as_slice())
+        .map_err(|_| error!(SettlerError::InvalidTransferRecipient))?;
+
+    let transfer_token = Pubkey::try_from(transfer.token.as_slice())
+        .map_err(|_| error!(SettlerError::InvalidTransferToken))?;
+
+    require_keys_eq!(transfer_recipient, recipient_account_info.key());
+    require_keys_eq!(transfer_token, token_account_info.key());
+    require_keys_eq!(recipient_ta.owner, recipient_account_info.key());
+    require_keys_eq!(recipient_ta.mint, token_account_info.key());
+    require_keys_eq!(user_ta.owner, user);
+    require_keys_eq!(user_ta.mint, token_account_info.key());
+
+    let cpi_accounts = TransferChecked {
+        authority: delegate.clone(),
+        from: user_ta_account_info.clone(),
+        mint: token_account_info.clone(),
+        to: recipient_ta_account_info.clone(),
+    };
+
+    let cpi_program = if *token_account_info.owner == anchor_spl::token::ID {
+        token_program.clone()
+    } else {
+        token_2022_program.clone()
+    };
+
+    let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, delegate_seeds);
+    token_interface::transfer_checked(cpi_ctx, transfer.amount, token_mint.decimals)
+}
+
+fn execute_transfers<'info>(
+    user: Pubkey,
     delegate: &AccountInfo<'info>,
     remaining_accounts: &[AccountInfo<'info>],
     intent_data: &SvmTransferIntentData,
@@ -49,48 +104,15 @@ fn execute_transfer<'info>(
     require_keys_eq!(token_2022_program.key(), anchor_spl::token_2022::ID);
 
     for transfer in &intent_data.transfers {
-        let token_account_info = next_account_info(&mut remaining_accounts_iter)?;
-        let recipient_account_info = next_account_info(&mut remaining_accounts_iter)?;
-        let recipient_ta_account_info = next_account_info(&mut remaining_accounts_iter)?;
-        let user_ta_account_info = next_account_info(&mut remaining_accounts_iter)?;
-
-        let mut token_data: &[u8] = &token_account_info.try_borrow_data()?;
-        let token_mint = IMint::try_deserialize(&mut token_data)?;
-
-        let mut recipient_ta_data: &[u8] = &recipient_ta_account_info.try_borrow_data()?;
-        let recipient_ta = ITokenAccount::try_deserialize(&mut recipient_ta_data)?;
-
-        let mut user_ta_data: &[u8] = &user_ta_account_info.try_borrow_data()?;
-        let user_ta = ITokenAccount::try_deserialize(&mut user_ta_data)?;
-
-        let transfer_recipient = Pubkey::try_from(transfer.recipient.as_slice())
-            .map_err(|_| error!(SettlerError::InvalidTransferRecipient))?;
-
-        let transfer_token = Pubkey::try_from(transfer.token.as_slice())
-            .map_err(|_| error!(SettlerError::InvalidTransferToken))?;
-
-        require_keys_eq!(transfer_recipient, recipient_account_info.key());
-        require_keys_eq!(transfer_token, token_account_info.key());
-        require_keys_eq!(recipient_ta.owner, recipient_account_info.key());
-        require_keys_eq!(recipient_ta.mint, token_account_info.key());
-        require_keys_eq!(user_ta.owner, *user);
-        require_keys_eq!(user_ta.mint, token_account_info.key());
-
-        let cpi_accounts = TransferChecked {
-            authority: delegate.clone(),
-            from: user_ta_account_info.clone(),
-            mint: token_account_info.clone(),
-            to: recipient_ta_account_info.clone(),
-        };
-
-        let cpi_program = if *token_account_info.owner == anchor_spl::token::ID {
-            token_program.clone()
-        } else {
-            token_2022_program.clone()
-        };
-
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, delegate_seeds);
-        token_interface::transfer_checked(cpi_ctx, transfer.amount, token_mint.decimals)?;
+        execute_transfer(
+            transfer,
+            delegate,
+            &mut remaining_accounts_iter,
+            delegate_seeds,
+            user,
+            token_program,
+            token_2022_program,
+        )?;
     }
 
     Ok(())
